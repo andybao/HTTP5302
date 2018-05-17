bb6168fa4637e4ab2b621ea1ea5c378a4f2aa479
andybao <wenyu.bao@gmail.com>
2018-05-17 09:31:04 -0400

    class demo for lab2 part1

---
+++ lab2/lab2.js
@@ -0,0 +1,58 @@
+//enums
+var months;
+(function (months) {
+    months[months["January"] = 0] = "January";
+    months[months["February"] = 1] = "February";
+    months[months["March"] = 2] = "March";
+    months[months["April"] = 3] = "April";
+    months[months["May"] = 4] = "May";
+    months[months["June"] = 5] = "June";
+    months[months["July"] = 6] = "July";
+    months[months["August"] = 7] = "August";
+    months[months["September"] = 8] = "September";
+    months[months["October"] = 9] = "October";
+    months[months["Novemever"] = 10] = "Novemever";
+    months[months["December"] = 11] = "December";
+})(months || (months = {}));
+var days;
+(function (days) {
+    days[days["Sunday"] = 0] = "Sunday";
+    days[days["Monday"] = 1] = "Monday";
+    days[days["Tuesday"] = 2] = "Tuesday";
+    days[days["Wednesday"] = 3] = "Wednesday";
+    days[days["Thursday"] = 4] = "Thursday";
+    days[days["Friday"] = 5] = "Friday";
+    days[days["Saturday"] = 6] = "Saturday";
+})(days || (days = {}));
+/*
+enum days {
+    Dinanche,
+    Lundi,
+    Mardi,
+    Mecredi,
+    Jeudi,
+    Vendredi,
+    Samedi
+}
+*/
+console.log("Months enum:");
+console.log(months);
+console.log('January: ' + months.January);
+console.log('Month 1: ' + months.January);
+//html elements
+var pTodayDate = document.getElementById("p--today-date");
+//today's date
+var today = new Date();
+console.log(today);
+//today's month
+console.log(today.getMonth());
+var todayMonth = months[today.getMonth()];
+//today's day of week
+console.log(today.getDay());
+var todayDayOfWeek = days[today.getDay()];
+//today's day of month
+console.log(today.getDate());
+//today's year
+console.log(today.getFullYear());
+//display today's date to page
+pTodayDate.innerHTML = "Today is " + todayDayOfWeek + ", " + todayMonth + ", " + today.getDate() + ", " + today.getFullYear();
---
+++ lab2/lab2.ts
@@ -0,0 +1,64 @@
+//enums
+enum months{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    Novemever,
+    December
+}
+
+enum days {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+/*
+enum days {
+    Dinanche,
+    Lundi,
+    Mardi,
+    Mecredi,
+    Jeudi,
+    Vendredi,
+    Samedi
+}
+*/
+console.log("Months enum:");
+console.log(months);
+console.log('January: ' + months.January);
+console.log('Month 1: ' + months.January);
+
+//html elements
+let pTodayDate = document.getElementById("p--today-date");
+
+//today's date
+let today : Date = new Date();
+console.log(today);
+
+//today's month
+console.log(today.getMonth());
+let todayMonth : string = months[today.getMonth()];
+
+//today's day of week
+console.log(today.getDay());
+let todayDayOfWeek : string = days[today.getDay()];
+
+//today's day of month
+console.log(today.getDate());
+
+//today's year
+console.log(today.getFullYear());
+
+//display today's date to page
+pTodayDate.innerHTML = `Today is ${ todayDayOfWeek}, ${ todayMonth }, ${ today.getDate() }, ${ today.getFullYear() }`;
