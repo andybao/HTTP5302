3b8ab4862aedc825941d64521ebb7c7eface99d9
andybao <wenyu.bao@gmail.com>
2018-05-25 00:22:45 -0400

    lab3 - part 2: It doesn't fix the google 10 limit

--- lab3/mp.ts
+++ lab3/mp.ts
@@ -8,7 +8,20 @@ interface LatLng {
 
 let Toronto : LatLng = { lat: 43, lng: -79.38};
 
+$.ajax({
+    url: './locations.json',
+    dataType: 'json',
+    success: function(data) {
+        for (let i of data) {
+            addMarkerWithGeocoder(i.address);
+        }
+    }
+});
+
 function initMap() {
+
+    let geocoder: object = new google.maps.Geocoder();
+
     map = new google.maps.Map(
         document.getElementById("google_map"),
         {
@@ -17,4 +30,29 @@ function initMap() {
             zoom: 8
         }
     );
+}
+
+function addMarkerWithGeocoder(address : string) {
+    let resultLatlng : LatLng = {lat: 0, lng: 0};
+
+    geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode(
+        {'address': address},
+        function (results, status) {
+
+            if (status == 'OK') {
+                resultLatlng.lat = results[0].geometry.location.lat();
+                resultLatlng.lng = results[0].geometry.location.lng();
+                addMarker(resultLatlng);
+            }
+        }
+    );
+}
+
+function addMarker (location) {
+    market = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
 }
